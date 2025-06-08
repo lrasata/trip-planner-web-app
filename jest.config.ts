f8a7f31950ff58e242102a9a4539689be4addf00
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-06-08 18:14:55 +0200

    chore: set up unit tests

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
+};
+
+export default config;
