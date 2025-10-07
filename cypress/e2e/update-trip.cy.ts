8c3621b6eb25d2a43d949c0b11ad4396bd068aef
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-10-07 16:27:44 +0200

    fix: cicd pipeline

--- cypress/e2e/update-trip.cy.ts
+++ cypress/e2e/update-trip.cy.ts
@@ -10,14 +10,14 @@ describe("Update trip", () => {
     // visit all-trips
     cy.contains("button", "Your trips").click();
     // created Trip is listed
-    cy.contains(tripName, { timeout: 3000 }).should("exist");
+    cy.contains(tripName, { timeout: 5000 }).should("exist");
 
     cy.get(`button[id="${tripName}-trip-manage-button"]`).click();
 
     // edit page appears
-    cy.contains("Manage your trip", { timeout: 3000 }).should("exist");
+    cy.contains("Manage your trip", { timeout: 10000 }).should("exist");
     // check trip name
-    cy.contains(tripName, { timeout: 3000 }).should("exist");
+    cy.contains(tripName, { timeout: 5000 }).should("exist");
     cy.get(`button[id="edit-trip-name-description-btn"]`).click();
 
     // check trip name and description in input
@@ -31,8 +31,8 @@ describe("Update trip", () => {
 
     cy.get(`button[id="save-trip-name-description-btn"]`).click();
 
-    cy.contains(`${tripName} updated`, { timeout: 3000 }).should("exist");
-    cy.contains("adding some description", { timeout: 3000 }).should("exist");
+    cy.contains(`${tripName} updated`, { timeout: 5000 }).should("exist");
+    cy.contains("adding some description", { timeout: 5000 }).should("exist");
 
     // Update departure and return date which are valid
     pickDate("edit-departure-date-input-button", 21);
